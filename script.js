60c76b2461596384c61a85de386e31690633c360
Tanvir Hossen Raju <103171957+tanvirhossen853@users.noreply.github.com>
2023-03-21 22:54:15 +0600

    Generate Color

---
+++ script.js
@@ -0,0 +1,25 @@
+var colors = [
+  "Orangered",
+  "Blue",
+  "Yellow",
+  "Green",
+  "Orange",
+  "Cyan",
+  "DodgerBlue",
+  "Violet",
+  "Navy",
+  "Purple",
+  "YellowGreen",
+  "OrangeRed",
+  "SlateBlue",
+  "Salmon",
+  "Crimson",
+  "HotPink",
+  "Magenta",
+];
+var index = 0;
+
+function color() {
+  if (index > colors.length - 1) index = 0;
+  document.body.style.backgroundColor = colors[index++];
+}
